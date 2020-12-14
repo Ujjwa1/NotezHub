7131946b619f6381ebe0e1f1ad278c073badd5ca
Ujjwal <meujjwal21@gmail.com>
2020-12-14 23:54:18 +0530

    NPM

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+$(document).click(function(){
+    if ($("#carousel-button").children("span").hasClass('fa-pause')) {
+                  $("#mycarousel").carousel('pause');
+                  $("#carousel-button").children("span").removeClass('fa-pause');
+                  $("#carousel-button").children("span").addClass('fa-play');
+              }
+              else if ($("#carousel-button").children("span").hasClass('fa-play')){
+                  $("#mycarousel").carousel('cycle');
+                  $("#carousel-button").children("span").removeClass('fa-play');
+                  $("#carousel-button").children("span").addClass('fa-pause');                    
+              }
+              $('#loginButton').click(function() {
+      $('#loginModal').modal('show');
+    });
+    $('#reserveButton').click(function() {
+      $('#reservationModal').modal('show');
+    });
+  });
